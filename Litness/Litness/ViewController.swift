6e2812ebf7b547f02f0021cbef0188d4f0ca70fb
igalfsg <igal@Macs-MacBook-Pro-99.local>
2017-05-01 20:45:10 -0400

    first push

---
+++ Litness/Litness/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Litness
+//
+//  Created by Igal on 5/1/17.
+//  Copyright © 2017 Igalfsg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
